d2444d33f3c400f27f88c8ca854dd0771a4a55c7
Rem M. <morrig407@gmail.com>
2024-06-08 22:41:12 +0700

    Add tests for nested

--- tests/test_make_diff.py
+++ tests/test_make_diff.py
@@ -1,4 +1,5 @@
 from gendiff.utils.make_diff import *
+from gendiff.utils.parse_file import parse_file
 
 import json
 import pytest
@@ -33,14 +34,54 @@ def make_format_diff_str():
 	return result
 
 
-def test_make_diff(make_test_data_1, make_test_data_2, make_test_result):
-	def _sort_by_name(data):
+@pytest.fixture
+def nested_data_1():
+	result = parse_file("tests/fixtures/make_diff_nested_1.yaml")
+	return result
+
+
+@pytest.fixture
+def nested_data_2():
+	result = parse_file("tests/fixtures/make_diff_nested_2.yaml")
+	return result
+
+
+@pytest.fixture
+def nested_data_expected_dict():
+	result = parse_file("tests/fixtures/nested_data_expected_dict.json")
+	return result
+
+
+@pytest.fixture
+def nested_data_expected_diff():
+	result = parse_file("tests/fixtures/nested_data_expected_diff.json")
+	return result
+
+
+def _sort_by_name(data):
 		return sorted(data, key=lambda x: x['name'])
+
+
+def test_make_diff(make_test_data_1, make_test_data_2, make_test_result):
 	result = make_diff(make_test_data_1, make_test_data_2)
 	result = _sort_by_name(result)
 	make_test_result = _sort_by_name(make_test_result)
 	assert result == make_test_result
 	assert make_diff({}, {}) == []
+
+
+def test_make_diff_nested(nested_data_1, nested_data_2, nested_data_expected_diff):
+	result = make_diff(nested_data_1, nested_data_2)
+	expected = nested_data_expected_diff
+	result = _sort_by_name(result)
+	expected = _sort_by_name(expected)
+	assert result == expected
+
+
+def test_make_and_format_diff_nested(nested_data_1, nested_data_2, nested_data_expected_dict):
+	result = make_and_format_diff(nested_data_1, nested_data_2)
+	expected = nested_data_expected_dict
+	assert result == expected
 
 
 def test_make_and_format_diff(make_test_data_1, make_test_data_2, make_format_diff_str):
