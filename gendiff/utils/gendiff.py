873791a7c2ebe14fd0535760bac6d359d66e9997
Rem M. <morrig407@gmail.com>
2024-06-15 23:17:25 +0700

    Move main logic from scripts

---
+++ gendiff/utils/gendiff.py
@@ -0,0 +1,23 @@
+from gendiff.utils.parse_file import parse_file
+from gendiff.utils.stylish import stylish, stylish_plain, json_formatter
+from gendiff.utils.make_diff import make_diff, format_diff
+
+
+def generate_diff(file_path1, file_path2, output_type=None, formatter=stylish):
+    file1_data = parse_file(file_path1)
+    file2_data = parse_file(file_path2)
+
+    match output_type:
+        case "json":
+            formatter = json_formatter
+        case "plain":
+            formatter = stylish_plain
+        case _:
+            formatter = stylish
+            output_type = "json"
+
+    result = format_diff(make_diff(file1_data, file2_data), type=output_type)
+    if formatter:
+        result = formatter(result)
+
+    return result
