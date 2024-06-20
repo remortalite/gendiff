2d03cbb63d28c53b8e621a4a144bb8d0f9227286
Rem M. <morrig407@gmail.com>
2024-06-20 18:24:13 +0700

    Create func parse() for file-parsing

--- gendiff/parse_file.py
+++ gendiff/parse_file.py
@@ -2,22 +2,24 @@ import json
 import yaml
 
 
-def parse_json(data):
-    return json.loads(data)
-
-
-def parse_yaml(data):
-    return yaml.safe_load(data)
+def parse(data, format):
+    match format:
+        case "json":
+            return json.loads(data)
+        case "yaml":
+            return yaml.safe_load(data)
+        case _:
+            raise ValueError("Wrong format!")
 
 
 def parse_file(filename):
     data = None
     if filename.endswith("json"):
         with open(filename) as f:
-            data = parse_json(f.read())
+            data = parse(f.read(), "json")
     elif filename.endswith("yml") or filename.endswith("yaml"):
         with open(filename) as f:
-            data = parse_yaml(f.read())
+            data = parse(f.read(), "yaml")
     else:
         raise ValueError(f"Wrong exception at `{filename}`")
     return data
