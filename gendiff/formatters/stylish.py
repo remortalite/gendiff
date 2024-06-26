67132711f8ba873a401e1101cf9b33da8e6dd1c8
Rem M. <morrig407@gmail.com>
2024-06-26 18:35:26 +0700

    Some changes

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -3,9 +3,17 @@ INDENT_SIZE = 4
 SHIFT_SIZE = 2
 
 
-def prepare_value(value):
+def prepare_value(value, *, level=1):
+    result = [""]
     if isinstance(value, dict):
-        return ""
+
+        nested_result = iter_(value,
+                              level+1)
+        if nested_result:
+            result[-1] += "{"
+            result.extend(nested_result)
+            result.append(f"{(level * INDENT_SIZE)  * INDENT_SYMBOL}" + f"}}")
+        return "\n".join(result)
     elif isinstance(value, bool):
         return str(value).lower()
     elif value is None:
@@ -26,21 +34,10 @@ def iter_(current_data, level=1):
     for key in current_data.keys():
         indent = indent_size_counted * INDENT_SYMBOL
         key = key
-        value = prepare_value(current_data[key])
+        value = prepare_value(current_data[key], level=level)
 
         result_str = f"{indent}{key}: {value}"
         result.append(result_str)
-
-        if isinstance(current_data[key], dict):
-
-            nested_result = iter_(current_data[key],
-                                  level + 1)
-
-            if nested_result:
-                result[-1] += "{"
-                result.extend(nested_result)
-
-            result.append(f"{indent}{SHIFT_SIZE * INDENT_SYMBOL}}}")
 
     return result
 
